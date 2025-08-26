1ef8faec2ce53225d1321f364780df2cf876f7d0
DanielDev2003 <everaldodaniel@hotmail.com>
2025-08-26 09:55:39 -0300

    GET por ID e PUT da entidade Nutricionistas

--- app.py
+++ app.py
@@ -8,13 +8,16 @@ from models.Usuario import Usuario
 from models.Nutricionista import Nutricionista
 from models.Dieta import Dieta
 from resources.UsuarioResource import UsuarioResource
-from resources.NutricionistaResource import NutricionistaResource
+from resources.NutricionistaResource import NutricionistaResource, NutricionistasResource
 from resources.DietaResource import DietaResource
 
 cors.init_app(app)
 
 api.add_resource(UsuarioResource, '/usuarios')
+
 api.add_resource(NutricionistaResource, '/nutricionistas')
+api.add_resource(NutricionistasResource, '/nutricionistas/<int:id>')
+
 api.add_resource(DietaResource, '/dietas')
 
 with app.app_context():
--- resources/NutricionistaResource.py
+++ resources/NutricionistaResource.py
@@ -1,6 +1,7 @@
 from flask_restful import Resource
 from flask import request
 from marshmallow import ValidationError
+from sqlalchemy.exc import IntegrityError, DataError
 
 from helpers.logging import logger
 
@@ -11,14 +12,16 @@ nutricionista_schema = NutricionistaSchema()
 nutricionistas_schema = NutricionistaSchema(many=True)
 
 class NutricionistaResource(Resource):
+
     def get(self):
-        logger.info("GET - Nutricionistas")
-        nutricionistas = Nutricionista.query.all()
-        return nutricionistas_schema.dump(nutricionistas), 200
+            logger.info("GET - Nutricionistas")
+            nutricionistas = Nutricionista.query.all()
+            return nutricionistas_schema.dump(nutricionistas), 200
 
     def post(self):
         logger.info("POST - Nutricionistas")
         data = request.get_json()
+
         try:
             validated = nutricionista_schema.load(data)
         except ValidationError as err:
@@ -26,5 +29,46 @@ class NutricionistaResource(Resource):
         
         nutricionista = Nutricionista(**validated)
         db.session.add(nutricionista)
-        db.session.commit()
+
+        try:
+            db.session.commit()
+        except IntegrityError:
+            db.session.rollback()
+            return {"error": "Violação de integridade (email ou crn já existentes)."}, 409
+        except DataError:
+            db.session.rollback()
+            return {"error": "Dados inválidos."}, 400
+        
         return nutricionista_schema.dump(nutricionista), 201
+
+class NutricionistasResource(Resource):
+     
+     def get(self, id):
+        logger.info(f"GET - Nutricionista id={id}")
+        nutricionista = Nutricionista.query.get_or_404(id, description="Nutricionista não encontrado")
+        return nutricionista_schema.dump(nutricionista), 200
+     
+     def put(self, id):
+        logger.info(f"PUT - Nutricionista id={id}")
+        nutricionista = Nutricionista.query.get_or_404(id, description="Nutricionista não encontrado")
+        data = request.get_json()
+        
+        try:
+            validated = nutricionista_schema.load(data)
+        except ValidationError as err:
+            return {"errors": err.messages}, 400
+
+        nutricionista.nome = validated["nome"]
+        nutricionista.email = validated["email"]
+        nutricionista.crn = validated["crn"]
+
+        try:
+            db.session.commit()
+        except IntegrityError:
+            db.session.rollback()
+            return {"error": "Violação de integridade (email ou crn já existentes)."}, 409
+        except DataError:
+            db.session.rollback()
+            return {"error": "Dados inválidos."}, 400
+
+        return nutricionista_schema.dump(nutricionista), 200
